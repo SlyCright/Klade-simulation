552a5ff058ed48347d4c2338a9f45f926320312c
SlyCright <sly.cright@gmail.com>
2026-02-28 20:45:59 +0300

    feat: add Arena class for specimen simulation environment

---
+++ src/main/java/site/klade/simulation/Arena.java
@@ -0,0 +1,63 @@
+package site.klade.simulation;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.math.Vector2;
+
+// import static site.klade.simulation.Genome.MIN_INITIAL_DISTANCE;
+
+public class Arena {
+
+    // TODO: that's simulation setting. Should move there
+    public static final float DISTANCE_TOLERANCE = 0.001f;
+
+    // private final Genome genome;
+
+    private final Engine engine = new Engine();
+
+    private final Entity specimen = new Entity();
+
+    // private final SpecimenPhysics specimenPhysics;
+
+    private final Vector2 center = new Vector2(0f, 0f);
+
+    private float fitness;
+
+    public Arena(
+            // Genome genome
+    ) {
+        // specimenPhysics = new SpecimenPhysics(genome);
+        // specimen.add(specimenPhysics);
+        engine.addEntity(specimen);
+        // engine.addSystem(new Movement());
+        // engine.addSystem(new Friction());
+        // this.genome = genome;
+    }
+
+    public void run() {
+        // float distanceFromCenter = center.dst(genome.getStartPositon());
+        // if (distanceFromCenter < MIN_INITIAL_DISTANCE) {
+        //     fitness = Float.MAX_VALUE;
+        //     genome.setFitness(fitness);
+        //     return;
+        // }
+        // var positionBefore = new Vector2(10_000f, 10_000f);
+        // var positionAfter = new Vector2();
+        // while (positionBefore.dst(positionAfter) > DISTANCE_TOLERANCE) {
+        //     positionBefore.set(positionAfter);
+        //     engine.update(0f);
+        //     positionAfter.set(specimenPhysics.getPositon());
+        //     this.fitness = center.dst(positionAfter);
+        // }
+        // genome.setFitness(fitness);
+    }
+
+    @SuppressWarnings("unused")
+    public void update() {
+        engine.update(0f);
+    }
+
+    public Entity getSpecimen() {
+        return specimen;
+    }
+}
